f0dc2eceb6c2d57d596a95726a545b47291f0564
Kanav Dhir <kdhir@bu.edu>
2014-12-10 18:27:40 -0800

    FileForm Complete

--- Lab8_KD/Lab8_KD/FileForm.Designer.cs
+++ Lab8_KD/Lab8_KD/FileForm.Designer.cs
@@ -30,58 +30,151 @@
         {
             this.currentFileBox = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.deleteFile = new System.Windows.Forms.Button();
+            this.addFile = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.intervalBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // currentFileBox
             // 
             this.currentFileBox.FormattingEnabled = true;
             this.currentFileBox.ItemHeight = 16;
-            this.currentFileBox.Location = new System.Drawing.Point(12, 38);
+            this.currentFileBox.Location = new System.Drawing.Point(12, 45);
             this.currentFileBox.Name = "currentFileBox";
             this.currentFileBox.Size = new System.Drawing.Size(661, 84);
             this.currentFileBox.TabIndex = 0;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.deleteFile);
+            this.groupBox1.Controls.Add(this.addFile);
             this.groupBox1.Location = new System.Drawing.Point(12, 167);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(302, 111);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Files";
             // 
-            // label1
+            // deleteFile
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.deleteFile.Location = new System.Drawing.Point(189, 45);
+            this.deleteFile.Name = "deleteFile";
+            this.deleteFile.Size = new System.Drawing.Size(77, 37);
+            this.deleteFile.TabIndex = 1;
+            this.deleteFile.Text = "Delete";
+            this.deleteFile.UseVisualStyleBackColor = true;
+            this.deleteFile.Click += new System.EventHandler(this.deleteFile_Click);
+            // 
+            // addFile
+            // 
+            this.addFile.Location = new System.Drawing.Point(33, 45);
+            this.addFile.Name = "addFile";
+            this.addFile.Size = new System.Drawing.Size(77, 37);
+            this.addFile.TabIndex = 0;
+            this.addFile.Text = "Add";
+            this.addFile.UseVisualStyleBackColor = true;
+            this.addFile.Click += new System.EventHandler(this.addFile_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(486, 191);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(107, 43);
+            this.showButton.TabIndex = 3;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(500, 187);
+            this.label2.Location = new System.Drawing.Point(447, 256);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Interval";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(695, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openCollectionToolStripMenuItem,
+            this.saveCollectionToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openCollectionToolStripMenuItem
+            // 
+            this.openCollectionToolStripMenuItem.Name = "openCollectionToolStripMenuItem";
+            this.openCollectionToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.openCollectionToolStripMenuItem.Text = "Open Collection";
+            this.openCollectionToolStripMenuItem.Click += new System.EventHandler(this.openCollectionToolStripMenuItem_Click);
+            // 
+            // saveCollectionToolStripMenuItem
+            // 
+            this.saveCollectionToolStripMenuItem.Name = "saveCollectionToolStripMenuItem";
+            this.saveCollectionToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.saveCollectionToolStripMenuItem.Text = "Save Collection";
+            this.saveCollectionToolStripMenuItem.Click += new System.EventHandler(this.saveCollectionToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // intervalBox
+            // 
+            this.intervalBox.Location = new System.Drawing.Point(507, 256);
+            this.intervalBox.Name = "intervalBox";
+            this.intervalBox.Size = new System.Drawing.Size(100, 22);
+            this.intervalBox.TabIndex = 6;
             // 
             // FileForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(695, 321);
+            this.Controls.Add(this.intervalBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showButton);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.currentFileBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FileForm";
             this.Text = "File Form";
-            this.Load += new System.EventHandler(this.FileForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,8 +184,18 @@
 
         private System.Windows.Forms.ListBox currentFileBox;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deleteFile;
+        private System.Windows.Forms.Button addFile;
+        private System.Windows.Forms.Button showButton;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openCollectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCollectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox intervalBox;
     }
 }
 
--- Lab8_KD/Lab8_KD/FileForm.cs
+++ Lab8_KD/Lab8_KD/FileForm.cs
@@ -7,19 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Lab8_KD
 {
     public partial class FileForm : Form
     {
+        public int currInterval;
+        private string openFileName;
         public FileForm()
         {
             InitializeComponent();
         }
+        private void addFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Image Files (*.jpg; *.gif; *.png; *.bmp)|*.jpg; *.gif; *.png; *.bmp|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            if (this.openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            string[] fileNames = this.openFileDialog1.FileNames;
+            string[] files = fileNames;
+            for (int i = 0; i < files.Length; i++)
+            {
+                string item = files[i];
+                this.currentFileBox.Items.Add(item);
+            }
+        }
 
-        private void FileForm_Load(object sender, EventArgs e)
+        private void deleteFile_Click(object sender, EventArgs e)
+        {
+            object[] files = new object[this.currentFileBox.SelectedItems.Count];
+            this.currentFileBox.SelectedItems.CopyTo(files, 0);
+            object[] updatedFiles = files;
+            for (int i = 0; i < updatedFiles.Length; i++)
+            {
+                object value = updatedFiles[i];
+                this.currentFileBox.Items.Remove(value);
+            }
+        }
+
+        private void openCollectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = " Collection Files (*.pix)|*.pix|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.currentFileBox.Items.Clear();
+                this.openFileName = this.openFileDialog1.FileName;
+                StreamReader streamReader = new StreamReader(this.openFileDialog1.OpenFile());
+                string item;
+                while ((item = streamReader.ReadLine()) != null)
+                {
+                    this.currentFileBox.Items.Add(item);
+                }
+                streamReader.Close();
+            }
+        }
+
+        private void saveCollectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.currentFileBox.Items.Count == 0)
+            {
+                MessageBox.Show("No file names to save.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return;
+            }
+            if (this.openFileName != null)
+            {
+                this.saveFileDialog1.FileName = this.openFileName;
+            }
+            else
+            {
+                this.saveFileDialog1.FileName = null;
+            }
+            if (this.saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.FileName = "";
+                openFileDialog1.Filter = "Image Files (*.jpg; *.gif; *.png; *.bmp)|*.jpg; *.gif; *.png; *.bmp|All Files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 1;
+                StreamWriter streamWriter = new StreamWriter(this.saveFileDialog1.OpenFile());
+                foreach (string value in this.currentFileBox.Items)
+                {
+                    streamWriter.WriteLine(value);
+                }
+                streamWriter.Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
         {
 
+            if (this.currentFileBox.Items.Count == 0)
+            {
+                MessageBox.Show("No images to show.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return;
+            }
+            try
+            {
+                currInterval = Int32.Parse(intervalBox.Text);
+                if (currInterval <= 0)
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please enter an integer time interval > 0.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            
+            SlideForm slideForm = new SlideForm { Owner = this };
+            slideForm.ShowDialog();
         }
     }
 }
