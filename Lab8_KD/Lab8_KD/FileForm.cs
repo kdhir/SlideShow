644110bca8bc98b8151dde1bb6554a4297cfd61b
Kanav Dhir <kdhir@bu.edu>
2014-12-10 17:21:50 -0800

    Setting up project

---
+++ Lab8_KD/Lab8_KD/FileForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8_KD
+{
+    public partial class FileForm : Form
+    {
+        public FileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FileForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
